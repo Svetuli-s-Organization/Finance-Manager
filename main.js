1a83825f3d943ecd647f272fbc66bd087485063d
Ftujio <svetlin954@abv.bg>
2017-09-02 17:40:14 +0300

    Created the main.js file.

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow(){
+	// Create browser window
+	win = new BrowserWindow({
+		width: 1200,
+		height: 800
+	});
+
+	win.loadURL(`file://${__dirname}/index.html`);
+
+	// Open Dev Tools
+	win.webContents.openDevTools();
+
+	win.on('closed', () => {
+		win = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if(process.platform !== 'darwin'){
+		app.quit();
+	}
+});
