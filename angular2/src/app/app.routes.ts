753315440fcd2476a6b115de4c1585914c0fe25c
Ftujio <svetlin954@abv.bg>
2017-09-05 19:21:54 +0300

    Changed the structure of the app.
    The "MainComponent" contains the sidenav and it's contents, while the "AppComponent" will hold the app logo and other stuff needed to be there throughout the app.

--- angular2/src/app/app.routes.ts
+++ angular2/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from "./components/main/main.component";
 
 const routes: Routes = [
-	// { path: '', component: MainComponent }
+	{ path: '', component: MainComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- angular2/src/app/components/main/main.component.ts
+++ angular2/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MdSidenavModule, MdListModule } from '@angular/material';
 
 @Component({
   selector: 'app-main',
