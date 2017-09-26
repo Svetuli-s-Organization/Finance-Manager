e13abf52ee0737d3776a066a5cdb1be665718d21
Ftujio <svetlin954@abv.bg>
2017-09-26 19:26:07 +0300

    Imported "RouterModule" and "Routes" to the root module.

--- angular2/src/app/app.module.ts
+++ angular2/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
