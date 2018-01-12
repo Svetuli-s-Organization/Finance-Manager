3ef06e576a6c624ab4bfae663b9bdba938841296
Ftujio <svetlin954@abv.bg>
2018-01-12 23:39:58 +0200

    Created component "StatisticsComponent".

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main/main.component';
 import { AddComponent } from './main/add/add.component';
+import { StatisticsComponent } from './main/statistics/statistics.component';
 
 const appRoutes = [
   { path: '', component: MainComponent },
   { path: 'add', component: AddComponent },
+  { path: 'statistics', component: StatisticsComponent },
 ];
 
 export const Routing = RouterModule.forRoot(appRoutes, { useHash: true });
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { AddComponent } from './add/add.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MainComponent, AddComponent],
+  declarations: [MainComponent, AddComponent, StatisticsComponent],
   exports: [MainComponent]
 })
 export class MainModule { }
