5050d2143619c7e6d50a669521a3b51b03ba2186
Ftujio <svetlin954@abv.bg>
2018-02-11 15:50:42 +0200

    Added validator for the date input.

--- src/app/main/add/add.component.ts
+++ src/app/main/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { BsDatepickerConfig, BsLocaleService } from 'ngx-bootstrap/datepicker';
@@ -13,6 +13,8 @@ export class AddComponent implements OnInit {
   inputDate: any;
   addForm: FormGroup;
 
+  @ViewChild('date') date: ElementRef;
+
   bsConfig: Partial<BsDatepickerConfig> = {
     containerClass: 'theme-blue',
     dateInputFormat: 'DD.MM.YYYY'
@@ -23,18 +25,23 @@ export class AddComponent implements OnInit {
   ngOnInit() {
     this.inputDate = new Date();
     let numberValidator = Validators.pattern(/^-?\d*(\.\d+)?$/);
-    let dateValidator = Validators.pattern(/^(0[1-9]|1[0-2])\.(0[1-9]|[1-2][0-9]|3[0-1])\.[0-9]{4}$/); // TODO: fix date validator
+    let dateValidator = Validators.pattern(/^(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.([0-9]){4}$/); // TODO: fix date validator
 
     this.addForm = new FormGroup({
-      date: new FormControl(this.inputDate, [dateValidator]),
+      date: new FormControl(this.inputDate),
       name: new FormControl('', [Validators.required]),
       price: new FormControl('', [numberValidator]),
     });
   }
 
   public addItem() {
+    let formatedDate: string = this.date.nativeElement.value;
+    let dateIsInvalid: boolean = !/^(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.([0-9]){4}$/.test(formatedDate);
+
+    if(dateIsInvalid) {
+      this.addForm.controls.date.setErrors({ 'incorrect': true });
+    }
     console.log(this.addForm.value);
-    console.log(this.addForm.valid);
   }
 
 }
