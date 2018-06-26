d89753f24e670d48e5c8734b7f10b5da26edfe29
Ftujio <svetlin954@abv.bg>
2018-06-26 22:44:26 +0300

    Added class property fileData.

--- src/app/api/file/file.service.ts
+++ src/app/api/file/file.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 
+import { FileData } from './file.data';
+
 @Injectable()
 export class FileService {
+
+  private fileData: FileData;
 
   constructor() { }
 
