f84f77707a2d5a752c6e51f4b35434871f21cc9f
Ftujio <svetlin954@abv.bg>
2018-07-08 21:28:40 +0300

    Added return types to the new methods.

--- src/app/api/file/file.service.ts
+++ src/app/api/file/file.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 
 import { FileData } from './file.data';
+import { MetaData } from './meta.data';
+import { Expense } from './expense';
+import { Category } from './category';
+import { Label } from './label';
+import { Tag } from './tag';
 
 @Injectable()
 export class FileService {
@@ -13,39 +18,39 @@ export class FileService {
     this.fileData = file;
   }
 
-  public getMetaData() {
+  public getMetaData(): MetaData {
     return this.fileData.metaData;
   }
 
-  public getExpenses() {
+  public getExpenses(): Expense[] {
     return this.fileData.data.expenses;
   }
 
-  public getExpense(id: number) {
+  public getExpense(id: number): Expense {
     return this.fileData.data.expenses.find(expense => expense.id === id);
   }
 
-  public getCategories() {
+  public getCategories(): Category[] {
     return this.fileData.data.categories;
   }
 
-  public getCategory(id: number) {
+  public getCategory(id: number): Category {
     return this.fileData.data.categories.find(category => category.id === id);
   }
 
-  public getLabels() {
+  public getLabels(): Label[] {
     return this.fileData.data.labels;
   }
 
-  public getLabel(id: number) {
+  public getLabel(id: number): Label {
     return this.fileData.data.labels.find(label => label.id === id);
   }
 
-  public getTags() {
+  public getTags(): Tag[] {
     return this.fileData.data.tags;
   }
 
-  public getTag(id: number) {
+  public getTag(id: number): Tag {
     return this.fileData.data.tags.find(tag => tag.id === id);
   }
 
