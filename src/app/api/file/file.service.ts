0d17a638be3bcc81455798e02e55d1a475d5d05a
Ftujio <svetlin954@abv.bg>
2018-07-08 21:26:05 +0300

    Added a bunch of get methods.

--- src/app/api/file/file.service.ts
+++ src/app/api/file/file.service.ts
@@ -13,4 +13,40 @@ export class FileService {
     this.fileData = file;
   }
 
+  public getMetaData() {
+    return this.fileData.metaData;
+  }
+
+  public getExpenses() {
+    return this.fileData.data.expenses;
+  }
+
+  public getExpense(id: number) {
+    return this.fileData.data.expenses.find(expense => expense.id === id);
+  }
+
+  public getCategories() {
+    return this.fileData.data.categories;
+  }
+
+  public getCategory(id: number) {
+    return this.fileData.data.categories.find(category => category.id === id);
+  }
+
+  public getLabels() {
+    return this.fileData.data.labels;
+  }
+
+  public getLabel(id: number) {
+    return this.fileData.data.labels.find(label => label.id === id);
+  }
+
+  public getTags() {
+    return this.fileData.data.tags;
+  }
+
+  public getTag(id: number) {
+    return this.fileData.data.tags.find(tag => tag.id === id);
+  }
+
 }
