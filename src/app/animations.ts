70d1f4b703701f359aa4a6421d8911173e902eee
Ftujio <svetlin954@abv.bg>
2018-09-03 23:24:52 +0300

    Improved the animation
    Fixed issue with width not being set while animating.

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,16 +1,15 @@
-import { trigger, state, style, animate, transition, query, group } from '@angular/animations';
+import { trigger, style, animate, transition, query, group } from '@angular/animations';
 
 export const navigationChange = trigger('navigationChange', [
   transition('* <=> *', [
-    query(':enter', [
-      style({
-        position: 'absolute'
-      })
-    ], { optional: true }),
     group([
       query(':enter', [
         style({ transform: 'translateX(-5%)' }),
-        animate('100ms ease-out', style({ transform: 'translateX(0)' }))
+        animate('250ms ease-out', style({ transform: 'translateX(0)' })),
+      ], { optional: true }),
+      query(':enter', [
+        style({ opacity: '0' }),
+        animate('300ms ease-out', style({ opacity: '1' })),
       ], { optional: true })
     ])
   ])
