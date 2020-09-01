6ec2908bd9ce0cee20adcfae9fac3507c9fd1118
Ftujio <svetlin.y8878@gmail.com>
2020-09-01 22:04:43 +0300

    Add WelcomeModule to AppModule declarations

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
+
+// Modules
+import { WelcomeModule } from './welcome/welcome.module';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		// Modules
+		WelcomeModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
