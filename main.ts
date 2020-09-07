d7ccbb51dadbb0e75c06b8e8c437b7284e139a8a
Ftujio <svetlin.y8878@gmail.com>
2020-09-08 01:39:33 +0300

    Open the BrowserWindow with option "nodeIntegration" enabled

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ app.whenReady().then(() => {
 	const win = new BrowserWindow({
 		width: 1600,
 		height: 900,
+		webPreferences: {
+			nodeIntegration: true,
+		}
 	});
 
 	win.loadURL('http://localhost:4200');
