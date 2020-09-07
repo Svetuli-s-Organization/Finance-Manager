adbc9871d07d0b66b9313367e6f3c0045e634f43
Ftujio <svetlin.y8878@gmail.com>
2020-09-07 23:56:11 +0300

    Add FileService to CoreModule providers

--- angular/src/app/modules/core/core.module.ts
+++ angular/src/app/modules/core/core.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Services
+import { FileService } from './services/file/file.service';
+
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule
 	],
-	providers: [],
+	providers: [
+		FileService,
+	],
 })
 export class CoreModule { }
