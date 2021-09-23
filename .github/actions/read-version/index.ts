76185fe0f88ab1238419aa05c9e940ab90e7587c
Ftujio <svetlin.y8878@gmail.com>
2021-09-23 18:54:40 +0300

    Print version

--- .github/actions/read-version/index.ts
+++ .github/actions/read-version/index.ts
@@ -7,6 +7,7 @@ try {
 	const file = readFileSync(packagePath, 'utf-8');
 	const json = JSON.parse(file);
 	const version: string = json.version;
+	console.log('!!!VERSION: ', version);
 	core.setOutput('version', version);
 } catch (error) {
 	core.setFailed(error.message);
