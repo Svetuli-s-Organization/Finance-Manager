cbd54da32292ea598f6b93a879197554e4701dd9
Ftujio <svetlin.y8878@gmail.com>
2021-10-05 02:09:26 +0300

    Handle creating/removing shortcuts on Windows when installing/uninstalling

--- electron-app/main.ts
+++ electron-app/main.ts
@@ -6,6 +6,11 @@ import path from 'path';
 
 import { isProd } from './environment';
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
 dotenv.config();
 updater();
 
