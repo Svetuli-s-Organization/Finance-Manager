26107a0ad46e81e7d8f464f67ff8df476314d944
Ftujio <svetlin.y8878@gmail.com>
2021-10-04 22:45:31 +0300

    Manually modify the package.json files, intead of using npm version
    The reason is the "npm version" command updates the package-lock.json files as well, which makes the node_modules caching in the CI/CD process obsolete.

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 const { chdir } = require('process');
 const path = require('path');
-const { exec } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
 
 const [type] = process.argv.slice(2);
 
@@ -8,13 +8,33 @@ if (!type) {
 	throw new Error('Missing argument type!');
 }
 
-const versionCommand = `npm version ${type} --no-git-tag-version`;
-
 /** Paths should be relative to this file */
-const bumpPaths = ['.', '/angular', '/electron-app'];
+const packageDirectoryPaths = ['.', '/angular', '/electron-app'];
 
-for (const bumpPath of bumpPaths) {
-	chdir(path.join(__dirname, bumpPath));
+for (const packageDirectoryPath of packageDirectoryPaths) {
+	const fullPackageDirectoryPath = path.join(__dirname, packageDirectoryPath);
+	chdir(fullPackageDirectoryPath);
+	const fullPackagePath = path.join(fullPackageDirectoryPath, 'package.json');
 	console.log(process.cwd());
-	exec(versionCommand);
+	const packageFile = readFileSync(fullPackagePath);
+	const packageObject = JSON.parse(packageFile);
+
+	let newVersion = getNewVersion(packageObject.version);
+	console.log(`v${newVersion}`);
+	const newPackageObject = { ...packageObject, version: newVersion };
+	const newPackage = JSON.stringify(newPackageObject, null, 2) + '\n';
+	writeFileSync(fullPackagePath, newPackage);
+}
+
+function getNewVersion(currentVersion) {
+	const [major, minor, patch] = currentVersion.split('.').map(s => +s);
+
+	switch (type) {
+		case 'major':
+			return `${major + 1}.0.0`;
+		case 'minor':
+			return `${major}.${minor + 1}.0`;
+		case 'patch':
+			return `${major}.${minor}.${patch + 1}`;
+	}
 }
