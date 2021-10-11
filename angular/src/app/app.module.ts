4c781eff83dc37b08cc25c220626f42e7844d52b
Ftujio <svetlin.y8878@gmail.com>
2021-10-12 00:20:51 +0300

    Move core services to folder "core"

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 // Modules
-import { CoreModule } from '@modules/core/core.module';
+import { CoreModule } from '@core/core.module';
 import { WelcomeModule } from '@modules/welcome/welcome.module';
 
 @NgModule({
