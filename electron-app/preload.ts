e129ac09e58a2c158d7932c7c6d78102b1c0bfd6
Ftujio <svetlin.y8878@gmail.com>
2021-10-13 00:15:36 +0300

    Expose only the on and send methods to the renderer process

--- electron-app/preload.ts
+++ electron-app/preload.ts
@@ -1,12 +1,33 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('electron', {
-	ipcRenderer: {
-		on: <T>(channel: string, listener: (data: T) => void) => {
-			ipcRenderer.on(channel, (_, data) => listener(data));
-		},
-		send: <T>(channel: string, data: T) => {
-			ipcRenderer.send(channel, data);
-		},
-	}
-});
+export type RendererAPIOnFn = <T>(channel: string, listener: (data: T) => void) => void;
+export type RendererAPISendFn = <T>(channel: string, data?: T) => void;
+
+/**
+ * An interface that describes the API that is exposed to the renderer process.
+ */
+export interface RendererAPI {
+	/**
+	 * Like the method `ipcRenderer.on`, but the listener function doesn't have the event
+	 * object passed to it.
+	 */
+	on: RendererAPIOnFn;
+	/**
+	 * Like the method `ipcRenderer.send`, but accepts only 1 data argument.
+	 */
+	send: RendererAPISendFn;
+}
+
+/**
+ * An object that contains the API that is exposed to the renderer process.
+ */
+const rendererAPI: RendererAPI = {
+	on: (channel, listener) => {
+		ipcRenderer.on(channel, (_, data) => listener(data));
+	},
+	send: (channel, data) => {
+		ipcRenderer.send(channel, data);
+	},
+}
+
+contextBridge.exposeInMainWorld('rendererAPI', rendererAPI);
