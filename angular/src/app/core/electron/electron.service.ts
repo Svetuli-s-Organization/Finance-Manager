f1e5089066a93068e273bd6d191d06172399eb85
Ftujio <svetlin.y8878@gmail.com>
2021-10-12 22:42:55 +0300

    Send event to the main process that the renderer is ready

--- angular/src/app/core/electron/electron.service.ts
+++ angular/src/app/core/electron/electron.service.ts
@@ -16,5 +16,6 @@ export class ElectronService {
 	constructor(@Inject(WINDOW) private window: Window) {
 		const { ipcRenderer } = this.window.electron;
 		this.ipcRenderer = ipcRenderer;
+		this.ipcRenderer.send('renderer-ready');
 	}
 }
