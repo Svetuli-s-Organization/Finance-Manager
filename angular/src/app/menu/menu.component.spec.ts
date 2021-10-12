b5f0cc10bbd98ca5584b5a338b7144150f6b6c2c
Ftujio <svetlin.y8878@gmail.com>
2021-10-13 01:00:05 +0300

    Test the mazimize and unmazimize functionality

--- angular/src/app/menu/menu.component.spec.ts
+++ angular/src/app/menu/menu.component.spec.ts
@@ -1,6 +1,9 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// External libraries
+import { Subject } from 'rxjs';
+
 // Components
 import { MenuComponent } from './menu.component';
 
@@ -37,7 +40,33 @@ describe('MenuComponent', () => {
 	});
 
 	describe(`class`, () => {
+		let electronService: ElectronService;
 
+		beforeEach(() => {
+			electronService = TestBed.inject(ElectronService);
+		});
+
+		it(`#ngOnInit should handle window maximize and unminimize events`, () => {
+			const onSubject: Subject<string> = new Subject();
+			const onSpy = spyOn(electronService, 'on').and.callFake((channel, listener) => {
+				onSubject.subscribe(testChannel => {
+					if (testChannel === channel) {
+						listener(null);
+					}
+				});
+			});
+
+			component.ngOnInit();
+			expect(component.maximized).toBeUndefined();
+			expect(onSpy.calls.allArgs()[0][0]).toEqual('window-maximize');
+			expect(onSpy.calls.allArgs()[1][0]).toEqual('window-unmaximize');
+
+			onSubject.next('window-maximize');
+			expect(component.maximized).toBeTrue();
+
+			onSubject.next('window-unmaximize');
+			expect(component.maximized).toBeFalse();
+		});
 	});
 
 	describe(`template`, () => {
