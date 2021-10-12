d22c48de2de5fbeae1fa6ebc2c4e2e2453a7e4b5
Ftujio <svetlin.y8878@gmail.com>
2021-10-12 22:42:31 +0300

    Expose only methods "on" and "send" in the ipcRenderer

--- electron-app/preload.ts
+++ electron-app/preload.ts
@@ -1,5 +1,12 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
-	ipcRenderer
+	ipcRenderer: {
+		on: <T>(channel: string, listener: (data: T) => void) => {
+			ipcRenderer.on(channel, (_, data) => listener(data));
+		},
+		send: <T>(channel: string, data: T) => {
+			ipcRenderer.send(channel, data);
+		},
+	}
 });
