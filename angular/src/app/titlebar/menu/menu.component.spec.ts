3b2269ff9a2ca053e5e172353e8ab4911493ef6d
Ftujio <svetlin.y8878@gmail.com>
2021-10-13 04:51:09 +0300

    Create unit tests for MenuComponent

--- angular/src/app/titlebar/menu/menu.component.spec.ts
+++ angular/src/app/titlebar/menu/menu.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
 // Components
 import { MenuComponent } from './menu.component';
@@ -22,7 +23,53 @@ describe('MenuComponent', () => {
 		fixture.detectChanges();
 	});
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
+	describe(`class`, () => {
+		const item1 = { id: 'item-1', name: 'Item 1' };
+		const item2 = { id: 'item-2', name: 'Item 2' };
+
+		it('should create', () => {
+			expect(component).toBeTruthy();
+		});
+
+		describe(`#ngOnChanges`, () => {
+			it(`shouldn't change #clickedMenuItem if #focusLose is falsy`, () => {
+				component.clickedMenuItem = item1;
+				component.ngOnChanges();
+				expect(component.clickedMenuItem).toEqual(item1);
+			});
+
+			it(`should set #clickedMenuItem to null when #focusLose Observable emits`, () => {
+				const focusLoseSubject: Subject<void> = new Subject();
+				component.focusLose = focusLoseSubject.asObservable();
+				component.ngOnChanges();
+				component.clickedMenuItem = item1;
+				focusLoseSubject.next();
+				expect(component.clickedMenuItem).toBeNull();
+			});
+		});
+
+		it(`#toggleActiveMenuItem should change #clickedMenuItem if it's truthy and it's different than the new one`, () => {
+			component.clickedMenuItem = null;
+			component.toggleActiveMenuItem(item1);
+
+			component.clickedMenuItem = item2;
+			component.toggleActiveMenuItem(item2);
+			expect(component.clickedMenuItem).toEqual(item2);
+
+			component.toggleActiveMenuItem(item1);
+			expect(component.clickedMenuItem).toEqual(item1);
+		});
+
+		it(`#toggleClickedMenuItem should toggle the value of #clickedMenuItem`, () => {
+			component.clickedMenuItem = null;
+			component.toggleClickedMenuItem(item1);
+			expect(component.clickedMenuItem).toEqual(item1);
+
+			component.toggleClickedMenuItem(item1);
+			expect(component.clickedMenuItem).toBeNull();
+
+			component.toggleClickedMenuItem(item2);
+			expect(component.clickedMenuItem).toEqual(item2);
+		});
 	});
 });
