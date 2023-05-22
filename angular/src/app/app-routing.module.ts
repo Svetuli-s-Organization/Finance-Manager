2854aecfdc757b59c341763ade5ada95aeea2c2a
Ftujio <svetlin.y8878@gmail.com>
2023-05-22 20:41:43 +0300

    Create MainModule

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'welcome', loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
   // { path: '**', redirectTo: '/welcome', pathMatch: 'full' },
 ];
 
